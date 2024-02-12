f1386eacd0c5ddb06859c6ad63f1cb63d4717827
Ильичев Евгений <37734257+asupstudent@users.noreply.github.com>
2024-02-12 09:02:21 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Шаблон React приложения.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
