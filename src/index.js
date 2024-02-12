438abfb4eaf44a16d51c2865c2dfb27af728b03c
asupstudent <asupstudent@yandex.ru>
2024-02-12 10:26:29 +0300

    Заканчивает задачу 2 - формирует структуру приложения

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import { React, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
