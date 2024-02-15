8c761d0257cad5e3f8d7c13c353ad70678b2dda8
asupstudent <asupstudent@yandex.ru>
2024-02-15 11:46:47 +0300

    Добавляет атрибуты в img

--- src/components/ui/star-card/star-card.jsx
+++ src/components/ui/star-card/star-card.jsx
@@ -6,7 +6,7 @@ function StarCard() {
   return (
     <article className='star-card'>
       <figure className='star-card__figure'>
-        <img src='/src/assets/sue.jpg'/>
+        <img src='/src/assets/sue.jpg' width={313} height={320} alt='Изображение кота'/>
       </figure>
       <Title size={TitleSize.SMALL}>Кошка Сью</Title>
     </article>
