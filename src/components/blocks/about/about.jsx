2569f64d84abd459927d17456e7b4ec002ba39a6
asupstudent <asupstudent@yandex.ru>
2024-02-16 12:58:23 +0300

    Проверяет деплой

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <section className='about'>
       <div className='about__wrapper'>
-        <Title size={TitleSize.BIG}>Первое в России котокафе</Title>
+        <Title size={TitleSize.BIG}>Первое в России котокафе!</Title>
         <p className='about__text'>Крупнейшее котокафе России, в котором живёт 50 кошек и котов, и каждый из них ищет новый дом. Животных можно гладить, фотографировать, играть с ними.</p>
         <p className='about__work-time'>Рабочее время с 8:00 до 23:00</p>
         <p className='about__address'>Санкт-Петербург,  набережная реки Карповки, дом 5, литера П</p>
