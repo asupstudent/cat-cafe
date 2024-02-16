2882971350dcd1f4c4636588026bbea1f579fe23
asupstudent <asupstudent@yandex.ru>
2024-02-16 13:43:28 +0300

    Переносит подключение шрифтов в app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageWrapper from '/src/components/layout/page-wrapper/page-wrapper';
 import './style.css';
+import '/src/fonts/inter-v13-cyrillic_latin-regular.woff2';
+import '/src/fonts/inter-v13-cyrillic_latin-700.woff2';
 
 export default function App() {
   return <PageWrapper />;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { React, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '/src/components/app/app';
-import './index.css';
-import '/src/fonts/inter-v13-cyrillic_latin-regular.woff2';
-import '/src/fonts/inter-v13-cyrillic_latin-700.woff2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
