40f7bf98830a723b5a72c15ca3465f4b568c0202
Евгений Ильичев <asupstudent@yandex.ru>
2024-03-01 12:37:58 +0300

    Стилизация главной страницы (styled-components) - Шаг 3.

---
+++ src/components/ui/styled/index.js
@@ -0,0 +1,3 @@
+export { default as Ul } from '/src/components/ui/styled/ul/ul';
+export { default as Li } from '/src/components/ui/styled/li/li';
+export { default as Section } from '/src/components/ui/styled/section/section';
